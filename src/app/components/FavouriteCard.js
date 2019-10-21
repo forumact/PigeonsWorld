d23892f78e75e3d1422e7df0b6ad96f962929cf9
forumact <forumact06@gmail.com>
2019-10-21 09:17:50 +0530

    flag item added

--- src/app/components/FavouriteCard.js
+++ src/app/components/FavouriteCard.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import avatar from '../assets/avatar.jpg';
+import { Link } from 'react-router-dom';
 
 class FavouriteCard extends Component {
     render() {
         return (
             <div className="product-item">
-                <a href="item-v1.html">
+                <Link to={`/product-details/${this.props.fav.nid}`}>
                     <figure className="product-preview-image small">
                         <img src={avatar} alt="product" />
                     </figure>
-                </a>
+                </Link>
                 <div className="product-info">
                     <a href="item-v1.html">
-                        <p className="text-header">Westeros Custom Clothing</p>
+                        <p className="text-header">{this.props.fav.title}</p>
                     </a>
-                    <p className="product-description">Lorem ipsum dolor sit urarde...</p>
+                    <p className="product-description">{this.props.fav.body}</p>
                     <a href="shop-gridview-v1.html">
                         <p className="category primary">PSD Templates</p>
                     </a>
@@ -27,13 +28,13 @@ class FavouriteCard extends Component {
                             </figure>
                         </a>
                         <a href="author-profile.html">
-                            <p className="text-header tiny">Johnny Fisher</p>
+                            <p className="text-header tiny">{this.props.fav.uname}</p>
                         </a>
                     </div>
                     <div className="metadata">
                         <div className="meta-item">
                             <span className="icon-bubble"></span>
-                            <p>12</p>
+                            <p>{this.props.fav.comment_count}</p>
                         </div>
                         <div className="meta-item">
                             <span className="icon-eye"></span>
@@ -41,7 +42,7 @@ class FavouriteCard extends Component {
                         </div>
                         <div className="meta-item">
                             <span className="icon-heart"></span>
-                            <p>105</p>
+                            <p>{this.props.fav.flag_count.favourites}</p>
                         </div>
                     </div>
                 </div>
@@ -50,7 +51,7 @@ class FavouriteCard extends Component {
                     </p>
                     <p className="text-header tiny">Dimensions: <span>4x6 Inches (with 0,25 bleed)</span>
                     </p>
-                    <p className="text-header tiny">
+                    {/* <p className="text-header tiny">
                         Tags:
                         <span>
                             <a href="/">custom</a>,
@@ -58,7 +59,7 @@ class FavouriteCard extends Component {
                             <a href="/">monsters</a>,
                             <a href="/">photoshop</a>, ...
                         </span>
-                    </p>
+                    </p> */}
                 </div>
                 <div className="author-data-reputation">
                     <p className="text-header tiny">Reputation</p>
